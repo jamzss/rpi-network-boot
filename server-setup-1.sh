f88de83c5c65ec9b69a8d8058f6c9f75e47e5180
jamzss <38468144+jamzss@users.noreply.github.com>
2021-03-04 17:20:28 +0000

    Split server setup scripts
    IP routes could not be properly added while not isolated from the network, so the scripts have been split in two.
    
    server-setup-1.sh is run when connected to the internet
    
    server-setup-2.sh is run when connected to an isolated switch

---
+++ server-setup-1.sh
@@ -0,0 +1,36 @@
+####
+####  server-setup-1.sh
+####
+
+# Run while connected to the internet.
+
+###
+### Download and install the RaspiOS image
+###
+
+cd /
+sudo apt install unzip kpartx dnsmasq nfs-kernel-server -y
+sudo mkdir -p /nfs/raspi1
+sudo wget https://downloads.raspberrypi.org/raspios_armhf/images/raspios_armhf-2021-01-12/2021-01-11-raspios-buster-armhf.zip
+unzip 2021-01-11-raspios-buster-armhf.zip
+sudo kpartx -a -v 2021-01-11-raspios-buster-armhf.img
+mkdir rootmnt
+mkdir bootmnt
+sudo mount /dev/mapper/loop0p2 rootmnt/
+sudo mount /dev/mapper/loop0p1 bootmnt/
+sudo cp -a rootmnt/* /nfs/raspi1/
+sudo cp -a bootmnt/* /nfs/raspi1/boot/
+
+
+###
+### Replace some boot files with updated versions
+###
+
+cd /nfs/raspi1/boot
+sudo rm start4.elf
+sudo rm fixup4.dat
+sudo wget https://github.com/Hexxeh/rpi-firmware/raw/master/start4.elf
+sudo wget https://github.com/Hexxeh/rpi-firmware/raw/master/fixup4.dat
+
+
+echo "Done!"
